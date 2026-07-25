e363a2d6a478dd881cd3deea500e7708a9fa7108
sonnycampbellhq <sonnycampbellhq@gmail.com>
2026-07-25 14:23:02 +0100

    did what the last commit said then tested web builds, it mostly worked but seemed to have some issues possibly only locally thinking it was a virus

--- Assets/Resources/Scripts/Player/CountessController.cs
+++ Assets/Resources/Scripts/Player/CountessController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using UnityEditor;
-//using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class CountessController : MonoBehaviour
